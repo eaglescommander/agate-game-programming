617dd7aabc1cff5f3ef57d1d3b4a61601e692b11
NADHIF ADYATMA PRAYOGA <nadhif.adyatma@ui.ac.id>
2021-09-17 09:13:48 +0700

    Finished chapter-5

---
+++ chapter-5/Angry Birds/Assets/Scripts/Owl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Owl : Bird
+{
+    public CircleCollider2D ColliderExplosion;
+    public Sprite fireSprite;
+    public float radius;
+
+    public void Explode()
+    {
+        if (State == BirdState.HitSomething)
+        {
+            GetComponent<CircleCollider2D>().radius = radius;
+            ColliderExplosion.enabled = true;
+            GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Static;
+            GetComponent<SpriteRenderer>().sprite = fireSprite;
+
+            StartCoroutine(DestroyAfter(0.3f));
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        _state = BirdState.HitSomething;
+        Explode();
+    }
+    
+}
