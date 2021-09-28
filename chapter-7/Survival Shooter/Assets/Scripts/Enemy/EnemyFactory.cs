c80fe97536d0a6df59cb26ef08d15f08d965a0d6
NADHIF ADYATMA PRAYOGA <nadhif.adyatma@ui.ac.id>
2021-09-28 08:44:35 +0700

    Finished chapter-7

---
+++ chapter-7/Survival Shooter/Assets/Scripts/Enemy/EnemyFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class EnemyFactory : MonoBehaviour, IFactory{
+
+    [SerializeField]
+    public GameObject[] enemyPrefab;
+
+    public GameObject FactoryMethod(int tag, Transform location)
+    {
+        GameObject enemy = Instantiate(enemyPrefab[tag], location.position, location.rotation);
+        return enemy;
+    }
+}
